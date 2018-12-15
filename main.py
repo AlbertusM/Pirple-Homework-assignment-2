b72ddf922da37a2869a06de72922b122c1a465be
Bertie <abmeiring@gmail.com>
2018-12-15 21:58:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# defining a function
+def genre(String):
+    Output = String
+    return Output
+
+
+Var = "Rock"
+print(Var)
+
+
+def TrackNumber(Number):
+    Output2 = Number
+    return Output2
+
+
+Var1 = 1
+print(Var1)
+
+
+def duration(Number2):
+    Output3 = Number2
+    return Output3
+
+
+Var3 = 3.45
+print(Var3)
